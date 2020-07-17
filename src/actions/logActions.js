30d623e9cf021c085ae74de401e7dc3cd57ff52e
unknown <ed.f.jones@gmail.com>
2020-07-17 12:02:55 -0400

    update log complete

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,13 @@
-import {GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG } from './types';
+import {
+    GET_LOGS, 
+    SET_LOADING, 
+    LOGS_ERROR, 
+    ADD_LOG, 
+    DELETE_LOG,
+    UPDATE_LOG, 
+    SET_CURRENT, 
+    CLEAR_CURRENT,
+ } from './types';
 
 export const getLogs = () => async dispatch => {
         try {
@@ -64,6 +73,48 @@ export const deleteLog = (id) => async dispatch => {
             payload: err.response.data
         });
     }
+};
+
+// upadte log on server
+export const updateLog = (log) => async dispatch => {
+    try {
+        setLoading();
+
+         const res = await fetch(`/logs/${log.id}`,{
+            method: 'PUT',
+            body: JSON.stringify(log),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await res.json();
+
+        dispatch({
+            type: UPDATE_LOG,
+            payload: data
+        });
+    } catch (err) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: err.response.data
+        });
+    }
+};
+
+// set current log
+export const setCurrent = (log) => {
+    return {
+        type: SET_CURRENT,
+        payload: log
+    };
+};
+
+// clear current log
+export const clearCurrent = () => {
+    return {
+        type: CLEAR_CURRENT,
+    };
 };
 
 // Set loading to true
